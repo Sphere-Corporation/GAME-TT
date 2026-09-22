da494269e7aeb3fb5982ba5cf122ea90067b8387
alshapton <alshapton@gmail.com>
2026-09-23 00:30:37 +0100

    Corrected first computer move to only place in a random corner

--- src/computer.asm
+++ src/computer.asm
@@ -8,15 +8,54 @@ GLOOPC                         ; Main Game Loop for playing vs Computer
         
         JSR     STR            ; Store A/B/X
 
-                               ; Check to see if this is the first move
-                               ; .......
+                               
+        LDAA    PIECES         ; Check to see if this is the first move
+        CMPA    #0
+        BEQ     .FRSTMV     
 
-        LDAA    COUNTER        ; Get random counter seed
-        ANDA    #$0F           ; Get lowest 4 bits (number 0 - 8)
-        INCA                   ; Add 1 to get the first random space.
+        ;LDAA    COUNTER        ; Get random counter seed
+        ;ANDA    #$0F           ; Get lowest 4 bits (number 0 - 8)
+        ;INCA                   ; Add 1 to get the first random space.
 
+.FRSTMV LDAA    COUNTER        ; Get random counter seed
+        ANDA    #%00000011     ; Get lowest 2 bits (number 0 - 3)
+        ; Minimise code 
+        ;
+        ;               1  |  2  |  3
+        ;             -----+-----+-----
+        ;               4  |  5  |  6
+        ;             -----+-----+-----
+        ;               7  |  8  |  9
+        ; 
+        ; AccA      Corner Mapped
+        ;   0             9
+        ;   1             1
+        ;   2             7
+        ;   3             3
+
+
+        CMPA    #0             ; Is the value 0?
+        BEQ     .CASE_0        ; Jump to CASE_0 if true
+        CMPA    #1             ; Is the value 1?
+        BEQ     .CASE_1        ; Jump to CASE_1 if true
+        CMPA    #2             ; Is the value 2?
+        BEQ     .CASE_2        ; Jump to CASE_2 if true
+
+; -----------------------------------------------------------------
+; If it wasn't 0, 1, or 2, it MUST be 3. The code naturally falls 
+; through to CASE_3, saving you an extra comparison instruction.
+; -----------------------------------------------------------------
+
+.CASE_3 LDAA    #3
+        BRA     .CONT
+.CASE_0 LDAA    #9
+        BRA     .CONT
+.CASE_1 LDAA    #1
+        BRA     .CONT
+.CASE_2 LDAA    #7
+                
                                ; Determine cursor position for piece
-        STAA    POSIT          ; Store piece position                       
+.CONT   STAA    POSIT          ; Store piece position                       
         CMPA    #1
         BEQ     .1
         CMPA    #2
